c91df22db73786388daba98d38f99485f2d16046
mustafayumrutepe <mstfyumrutepe@gmail.com>
2024-08-08 03:00:54 +0300

    test methods are collected in a single package

--- src/Tests/TestMethods.java
+++ src/Tests/TestMethods.java
@@ -1,20 +1,20 @@
-package Mustafa;
+package Tests;
 
 import ElementsPage.Elements;
 import ElementsPage.Parent;
 import Utility.BaseDriver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 
-public class mustafa extends BaseDriver {
+public class TestMethods extends BaseDriver {
 
     @Test(priority = 1 , groups = {"Smoke Test" , "UserRegistiration"})
     public void Register(){
@@ -141,5 +141,69 @@ public class mustafa extends BaseDriver {
         }
         Assert.assertEquals(check, true,"element not found in list");
     }
+
+
+
+
+    @Test(priority = 3,dataProvider = "userData",dataProviderClass = TestMethods.class,groups = {"Smoke Test","Login Test"})
+    public void NegativeAndPositiveLoginCombinations(String email, String password){
+        Elements el=new Elements();
+
+        el.clickFunction(el.getLogin());
+        el.sendKeysFunction(el.getEmail(),email);
+        el.sendKeysFunction(el.getPassword(),password);
+        el.clickFunction(el.getLoginButtonCCC());
+
+        if (email.equals("rkaya@gmail.com")&&password.equals("12345rk")){
+            Assert.assertTrue(el.getMyAccount().isDisplayed());
+            System.out.println("giriş Başarılı");
+            el.clickFunction(el.getLogout());
+        }else{
+            System.out.println("giriş Hatalı");
+
+        }
+        wait.until(ExpectedConditions.elementToBeClickable(el.getLogin()));
+
+
+
+    }
+    @DataProvider
+    public Object[][] userData() {
+        Object[][] data={
+                { "invalid1@example.com", "invalidpassword1", },
+                { "rkaya@gmail.com"     , "invalidpassword2", },
+                { "invalid3@example.com", "12345rk"         , },
+                { "rkaya@gmail.com", "12345rk"              , }
+        };
+        return data;
+    }
+
+    @Test(priority = 4 , groups = {"UI Testing" , "TAB Menu"})
+    public void GiftOrderingProcess(){
+        LoginTest("rkaya@gmail.com","12345rk");
+        Elements el=new Elements();
+        el.clickFunction(el.getGiftCardSec());
+        el.clickFunction(el.getPhsicCARD50());
+        el.sendKeysFunction(el.getRecipientName(),"Ekrem İMARO");
+        el.sendKeysFunction(el.getMessageBox(),"Mustafa Sarıgül vurdu ve Gol oldu. GGG000000000000000000000000000000000000000000000000000000000000llll");
+        el.clickFunction(el.getLastAddButton());
+        el.assertionFunction(el.getBeenAdded(),"been added");
+
+    }
+    @Parameters("text")
+    @Test(priority = 5,groups = {"UI Testing","Search","Smoke","Regression"})
+    public void ParameterSearchProcess(String text){
+        LoginTest("r2kaya@gmail.com","12345rk");
+        Elements el=new Elements();
+        el.sendKeysFunction(el.getSearchBox(),text);
+        Actions actions = new Actions(BaseDriver.driver);
+        actions.sendKeys(Keys.ENTER).build().perform();
+        el.assertionFunction(el.getPicAdoPhoto(),"adobe photoshop");
+
+
+
+
+    }
+
 }
 
